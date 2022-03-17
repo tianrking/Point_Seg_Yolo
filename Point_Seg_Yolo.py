9682c7051620fc42eb9598581eac533db38cc860
tianrking <tian.r.king@gmail.com>
2022-03-17 23:49:48 +0800

    Create Point_Seg_Yolo.py

---
+++ Point_Seg_Yolo.py
@@ -0,0 +1,90 @@
+from final_integration import Myinput
+import os
+import matplotlib.pyplot as plt
+from os import listdir
+from PIL import Image
+
+input_filepath = "/storage/lol/gg/yolov5/Point_Seg_data/"
+output_filepath = "/storage/lol/gg/yolov5/Point_Seg_data_Output/"
+output_k = len(os.listdir(output_filepath)) + 1  
+output_filepath = output_filepath + str(output_k) + "/"
+# print(output_filepath)
+
+class b(object):
+    def __init__(self,pic1,pic2,pic3,pic4,output_dir=1):
+        self.pic1 = pic1
+        self.pic2 = pic2
+        self.pic3 = pic4
+        self.pic4 = pic3
+        self.output_dir = output_dir
+
+def merge(GG):
+
+    input_filepath = GG.pic1
+    all_fusion_folder_path = GG.pic2
+    single_fusion_folder_path = GG.pic3
+    output_dir = GG.output_dir
+
+    im_list = [GG.pic1, GG.pic2,  GG.pic3, GG.pic4]
+    im_list =  [Image.open(fn) for fn in im_list]
+
+    ims = []
+
+    for i in im_list:
+        new_img = i.resize((1280, 1280), Image.BILINEAR)
+        ims.append(new_img)
+
+    width, height = ims[0].size
+
+    result = Image.new(ims[0].mode, (height * len(ims),width))
+
+    for i, im in enumerate(ims):
+        result.paste(im, box=(i * height, 0))
+    
+    result.save(output_dir)
+
+
+
+k=0
+for i in range(0,len(os.listdir(input_filepath+"0_images"))):
+
+    image_path = input_filepath + "0_images/%s.jpg"%str(i)
+    image_yolo_path = input_filepath + "exp6_image_data/%s.jpg"%str(i)
+
+    bin_path = input_filepath + "exp6_bin_data/%s.bin"%str(k)
+    # bin_path = input_filepath + "exp6_bin_data/"
+
+    calib_text_path_final = input_filepath + "calib/1.txt"
+    position_path = input_filepath + "poisition_data/%s.txt"%str(k)
+
+    seg_bin_folder_path = output_filepath + "seg_bin/"
+    single_fusion_folder_path = output_filepath + "single_fusion/"
+    all_fusion_folder_path = output_filepath + "all_fusion/"
+    all_in_one_path = output_filepath + "all_in_one/"
+
+    try:
+        os.makedirs(seg_bin_folder_path)
+        os.makedirs(single_fusion_folder_path)
+        os.makedirs(all_fusion_folder_path)
+        os.makedirs(all_in_one_path)
+    except:
+        input_message = Myinput()
+        input_message.data_input(image_path, bin_path, calib_text_path_final, position_path)
+        input_message.data_output(seg_bin_folder_path,single_fusion_folder_path,all_fusion_folder_path)
+        input_message.seg_point()
+
+        GG=b(
+            image_path,
+            image_yolo_path,
+            single_fusion_folder_path + "%s.jpg"%str(i),
+            all_fusion_folder_path + "%s.jpg"%str(i),
+            all_in_one_path + "%s.jpg"%str(i)
+            )
+        merge(GG)
+
+        k = k + 1
+
+
+    print(i)
+
+print("success")
